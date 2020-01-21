07f3c22654c59c742e7b1a0098a39d8ce6dcc099
nicholas-russell <nrussell97@outlook.com>
2020-01-21 20:34:24 +1100

    Commit

--- Site/views.py
+++ Site/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from API.models import Incidental
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("This is the index for site!")
+    return HttpResponse(Incidental.objects.get())
 
