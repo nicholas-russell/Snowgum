8499dab6125462a3546d93e2be25fed116a6743b
nicholas-russell <nrussell97@outlook.com>
2020-02-04 21:12:09 +1100

    Customised Incidentals admin page listing

--- API/admin.py
+++ API/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Incidental
-# Register your models here.
 
-admin.site.register(Incidental)
+
+class IncidentalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_obs', 'loc_lat', 'loc_lng', 'image_apr', 'verified', 'email', 'contact')
+
+
+admin.site.register(Incidental, IncidentalAdmin)
