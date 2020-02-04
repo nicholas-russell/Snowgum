f9f8e4782034da464f327ac139e6500456f7749d
nicholas-russell <nrussell97@outlook.com>
2020-02-04 20:52:32 +1100

    Implemented login/logout pages

--- Snowgum/urls.py
+++ Snowgum/urls.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 from django.urls import path, include
 from . import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Site.urls')),
     path('api/', include('API.urls')),
+    path('login/', views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(template_name='auth/logout.html', next_page='/'), name='logout'),
 ]
 
 if settings.DEBUG:
