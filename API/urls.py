300ab1fd945ce4d4c271920fde8cf6101880135e
nicholas-russell <nrussell97@outlook.com>
2020-02-04 15:16:54 +1100

    Tidying

--- API/urls.py
+++ API/urls.py
@@ -1,18 +1,6 @@
 from . import views
-from django.urls import path, include
+from django.urls import path
 
-# Endpoints
-# /api/inc
-#   - GET: get all incidentals
-#   - POST: make new incidental
-# /api/inc/##
-#   - GET: gets details of individual incidental
-#   - PUT/PATCH: update (needs user middleware)
-#   - DELETE: deletes (needs user middleware)
-# /api/inc/##/verify
-#   - POST: verifies (needs user middleware)
-# /api/inc/##/approve_image
-#   - POST: approves image (needs user middleware)
 
 app_name = 'api'
 urlpatterns = [
