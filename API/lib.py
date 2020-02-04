51e910a2adfbdaa63805c988fa0b7be2fa097e30
nicholas-russell <nrussell97@outlook.com>
2020-02-04 21:57:10 +1100

    Made factory to randomly generate entries

--- API/lib.py
+++ API/lib.py
@@ -1,7 +1,9 @@
 import os
+import string
 import uuid
 from django.utils.deconstruct import deconstructible
-
+from random import randint
+import random
 
 @deconstructible
 class RandomFileName(object):
@@ -12,3 +14,52 @@ class RandomFileName(object):
         # @note It's up to the validators to check if it's the correct file type in name or if one even exist.
         extension = os.path.splitext(filename)[1]
         return self.path % (uuid.uuid4().hex, extension)
+
+
+domains = ["hotmail.com", "gmail.com", "aol.com", "mail.com" , "mail.kz", "yahoo.com"]
+
+letters = string.ascii_lowercase[:12]
+
+
+def get_random_domain(domains):
+    return random.choice(domains)
+
+
+def get_random_name(letters, length):
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def generate_random_email(length):
+    return get_random_name(letters, length) + '@' + get_random_domain(domains)
+
+
+def generate_random_date():
+    return str(randint(2018, 2020)) + "-" + str(randint(1, 12)) + "-" + str(randint(1, 28))
+
+
+def generate_random_ip():
+    return str(randint(000, 999)) + "." + \
+           str(randint(000, 999)) + "." + \
+           str(randint(000, 999)) + "." + \
+           str(randint(000, 999))
+
+
+def generate_random_image():
+    return "test_images/" + str(random.randint(0, 16)) + ".jpg"
+
+
+def generate_random_lat():
+    return random.uniform(-36.256084, -36.788364)
+
+
+def generate_random_lng():
+    return random.uniform(147.949859, 148.371674)
+
+
+def random_data():
+    rtn = {'date_obs': generate_random_date(), 'description': "---This is a computer generated record---",
+           'email': generate_random_email(10), 'loc_lat': generate_random_lat(), 'loc_lng': generate_random_lng(),
+           'contact': bool(random.getrandbits(1)), 'image_apr': bool(random.getrandbits(1)),
+           'verified': bool(random.getrandbits(1)), 'verified_by': "computer", 'entered_ip': generate_random_ip(),
+           'image': generate_random_image()}
+    return rtn
--- API/urls.py
+++ API/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('inc/<int:inc_id>/approve-image', views.approve_image, name='inc_apr_img'),
     path('inc/<int:inc_id>/verify', views.verify, name='inc_verify'),
     path('inc/dt', views.IncidentalListJson.as_view(), name='inc_dt'),
-    path('auth/', views.AuthView.as_view())
+    path('auth/', views.AuthView.as_view()),
+    path('factory/<int:number>', views.factory)
 ]
